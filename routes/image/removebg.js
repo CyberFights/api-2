7caef4f46dc78ee26264ba81905525292fb0f103
CyberFights <shadowjer011@gmail.com>
2025-07-21 04:35:46 -0400

    Create removebg.js

---
+++ routes/image/removebg.js
@@ -0,0 +1,7 @@
+module.exports = {
+  path: '/image/removebg',
+  code: `
+  $request[https://api.remove.bg/v1.0/removebg;{"image_url": "$getQuery[image]"};Accept: image/*;X-API-Key: YVUZWmaLju7DgEuiGoxftXvU;Content-Type: application/json]
+  $send[200;other;$getData[$default]]
+  `
+}
