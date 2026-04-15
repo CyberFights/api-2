533e010e7f11a7f9a854a7b0997c50ad7a26e916
CyberFights <shadowjer011@gmail.com>
2026-04-15 16:38:16 -0400

    Update progressbar.js

--- routes/image/progressbar.js
+++ routes/image/progressbar.js
@@ -1,6 +1,7 @@
 module.exports = {
   path: '/image/progressbar',
   code: `
+  $registerFont[./assets/fonts/VtksDura3D-P12r.ttf;dura]
   $createCanvas[400;100]
   
   $color[#000000]
@@ -11,8 +12,8 @@ module.exports = {
   $drawImage[bar;0;0;$math[($getQuery[xp]/$getQuery[req])*400];100;50]]
   
   $color[#00ff00]
-  $font[25;Arial;bold]
-  $drawText[$getQuery[type]: $getQuery[xp]/$getQuery[req];50;20;200;200]
+  $font[25;dura]
+  $drawText[$getQuery[type]: $getQuery[xp]/$getQuery[req];20;20;300;50]
   $send[200;canvas;$default]
   `
 }
