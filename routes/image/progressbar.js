98cb905433f58aabce5fd6915815f8a408ec4cbd
CyberFights <shadowjer011@gmail.com>
2026-04-15 16:10:40 -0400

    Update progressbar.js

--- routes/image/progressbar.js
+++ routes/image/progressbar.js
@@ -4,11 +4,11 @@ module.exports = {
   $createCanvas[400;100]
   
   $color[#000000]
-  $drawRect[0;0;400;100;100]
+  $drawRect[0;0;400;100;50]
   $if[$getQuery[color-to]==undefined;
   $color[#00ff00]
-  $drawRect[0;0;$math[($getQuery[xp]/$getQuery[req])*400];100;100];$loadImage[bar;link;https://api-1-production-1161.up.railway.app/image/multigradient?color-to=$getQuery[color-to]&color-from=$getQuery[color-from]]
-  $drawImage[bar;0;0;$math[($getQuery[xp]/$getQuery[req])*400];100;100]]
+  $drawRect[0;0;$math[($getQuery[xp]/$getQuery[req])*400];100;50];$loadImage[bar;link;https://api-1-production-1161.up.railway.app/image/multigradient?color-to=$getQuery[color-to]&color-from=$getQuery[color-from]]
+  $drawImage[bar;0;0;$math[($getQuery[xp]/$getQuery[req])*400];100;50]]
   
   $color[#ffffff]
   $font[30;Arial;bold]
