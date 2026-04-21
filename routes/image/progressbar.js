1de0db9b906b2a8665074101770918143a2c5e6d
CyberFights <shadowjer011@gmail.com>
2026-04-21 07:02:17 -0400

    Update progressbar.js

--- routes/image/progressbar.js
+++ routes/image/progressbar.js
@@ -2,7 +2,7 @@ module.exports = {
   path: '/image/progressbar',
   code: `
   $registerFont[./assets/fonts/VtksDura3D-P12r.ttf;dura]
-  $createCanvas[400;100]
+  $createCanvas[400;350]
   
   $color[#000000]
   $drawRect[0;0;400;100;50]
@@ -10,6 +10,16 @@ module.exports = {
   $color[#00ff00]
   $drawRect[0;0;$math[($getQuery[xp]/$getQuery[req])*400];100;50];$loadImage[bar;link;https://api-1-production-1161.up.railway.app/image/multigradient?color-to=$getQuery[color-to]&color-from=$getQuery[color-from]]
   $drawImage[bar;0;0;$math[($getQuery[xp]/$getQuery[req])*400];100;50]]
+  $if[$getQuery[xp2]!=undefined;
+  $color[#000000]
+  $drawRect[0;100;400;100;50]
+  $loadImage[bar;link;https://api-1-production-1161.up.railway.app/image/multigradient?color-to=$getQuery[color-to2]&color-from=$getQuery[color-from2]]
+  $drawImage[bar;0;100;$math[($getQuery[xp2]/$getQuery[req2])*400];100;50];]
+    $if[$getQuery[xp3]!=undefined;
+  $color[#000000]
+  $drawRect[0;200;400;100;50]
+  $loadImage[bar;link;https://api-1-production-1161.up.railway.app/image/multigradient?color-to=$getQuery[color-to3]&color-from=$getQuery[color-from3]]
+  $drawImage[bar;0;200;$math[($getQuery[xp3]/$getQuery[req3])*400];100;50];]
   
   $color[#00ff00]
   $font[25;dura]
